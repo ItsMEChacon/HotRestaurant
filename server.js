841788db5e4ae340e33bb8d9b0a316f0b8ca49ef
Indervir Singh <iindervirsingh@gmail.com>
2021-03-04 20:40:22 -0800

    added

--- server.js
+++ server.js
@@ -20,8 +20,12 @@ const email = document.querySelector("#email");
 const id = document.querySelector("#id");
 const phoneNumber = document.querySelector("#phoneNumber");
 
+// Get the HTML for the tables and wait list
+const waitListDiv = document.querySelector("#waitListSection");
+const tableSectionDiv = document.querySelector("#tableSection");
+
 // Give it an action when clicked
-submitButton.on("click", addReservation(event))
+submitButton.on("click", addReservation(event));
 // Adding reservation
 const addReservation = (event) => {
 
@@ -34,31 +38,49 @@ const addReservation = (event) => {
         phoneNumber: phoneNumber.val()
     };
 
-    app.post("/tables", reservation, () => {
 
-        // If a spot is available then:
-        // add to table array
-        if (reservedTables.length <= 5) {
-            reservedTables.push(reservation);
-            addToTable(reservation);
-            console.log("Your reservation had been booked!");
-        }
-        // Else:
-        // add to waiting list array
-        else {
-            waitingList.push(reservation);
-            addToWaitList(reservation);
-            console.log("Sorry you have been added to our wait list..please wait your turn.");
-        }
+    // If a spot is available then:
+    // add to table array
+    if (reservedTables.length < 5) {
+        reservedTables.push(reservation);
+        addToTable(reservation);
+        console.log("Your reservation had been booked!");
+    }
+    // Else:
+    // add to waiting list array
+    else {
+        waitingList.push(reservation);
+        addToWaitList(reservation);
+        console.log("Sorry you have been added to our wait list..please wait your turn.");
+    }
 
 
-    });
 };
 
 const addToTable = (reservation) => {
 
+    const {name, email, id, phoneNumber } = reservation;
+
+    let listItem = $("<li>");
+
+    listItem.append(
+      $("<h2>").text("Table #" + (i + 1)),
+      $("<hr>"),
+      $("<h2>").text("ID: " + id),
+      $("<h2>").text("Name: " + name),
+      $("<h2>").text("Email: " + email),
+      $("<h2>").text("Phone: " + phoneNumber)
+    );
+
+
+    tableSectionDiv.append(listItem);
 };
 
 const addToWaitList = (reservation) => {
+
+    const {name, email, id, phoneNumber } = reservation;
+
+    
+
 
 }
