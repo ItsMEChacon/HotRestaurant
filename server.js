8029093a8517d35009de2eaca3f518b97b3c5b57
Manny Chacon <chmanuel2030@gmail.com>
2021-03-04 19:55:47 -0800

    fixed pathing

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3500;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send ('Table reservation server live!')
-})
+const reservedTables = ['randomdata'];
+const waitList = ['declined'];
 
-app.listen(PORT, () => console.log (`app listening on PORT ${PORT}`));
+app.get('/home', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
+
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+
+app.get('/reserve/:reserve', (req, res) => { 
+    const list = req.params.reservedTables
+    
+    console.log(list)
+});
+
+app.listen(PORT, () => console.log (`App listening on PORT ${PORT}`));
+
